fb928edac7c2fb890b1df90834637d97624d242d
David <101667551+Dejwik@users.noreply.github.com>
2022-04-24 11:33:03 +0200

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,21 @@
+Config = {}
+Config.SeeOwnLabel = true
+Config.SeeDistance = 70
+Config.TextSize = 1.2
+Config.ZOffset = 1.2
+Config.NearCheckWait = 500
+Config.TagByPermission = false --Using xPlayer.getPermissions() which is deprecated method for old ESX
+Config.GroupLabels = {
+    helper = "HELPER",
+    mod = "~g~MODERATOR",
+    admin = "~r~Admin",
+    superadmin = "~p~Managment",
+}
+
+Config.PermissionLabels = {
+    [1] = "HELPER",
+    [2] = "~g~MODERATOR",
+    [3] = "~r~ADMIN",
+    [4] = "~r~GOD",
+    [5] = "~r~GOD",
+}
